35e89c1e279127308a3c8cbfff1b37c3a42b054b
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-05-01 05:36:32 +0600

    itemDetails page is designed

--- src/Components/InventoryItem/InventoryItem.jsx
+++ src/Components/InventoryItem/InventoryItem.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const InventoryItem = () => {
-    const [items, setItems] = useState([]);
-    const navigate = useNavigate()
+  const [items, setItems] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     fetch("http://localhost:4000/laptops")
       .then((res) => res.json())
@@ -13,13 +13,19 @@ const InventoryItem = () => {
     <div className="bg-[#0b0b0b]">
       <div className="container mx-auto py-16">
         <h1 className="text-3xl font-bold text-center mb-16 text-white">
-         
           Inventory Items
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-8 lg:gap-10">
           {items.map((item) => (
-            <div key={item._id} className="mb-4 bg-slate-200 shadow-sky-300 shadow-lg">
-              <img className="h-96 w-full object-cover" src={item.image} alt="" />
+            <div
+              key={item._id}
+              className="mb-4 bg-slate-200 shadow-sky-300 shadow-lg"
+            >
+              <img
+                className="h-96 w-full object-cover"
+                src={item.image}
+                alt=""
+              />
               <div className="py-4 px-4">
                 <h3 className="text-2xl font-bold mb-3">{item.name}</h3>
                 <p>
@@ -35,13 +41,17 @@ const InventoryItem = () => {
                   {item.supplierName}
                 </p>
                 <p className="text-xl font-thin">{item.description}</p>
-                <button onClick={() => navigate(`/itemdetails/${item._id}`)} className="py-3 px-8 bg-blue-600 text-white rounded mt-4">Stock Update</button>
+                <button
+                  onClick={() => navigate(`/itemdetails/${item._id}`)}
+                  className="py-3 px-8 bg-blue-600 text-white rounded mt-4"
+                >
+                  Stock Update
+                </button>
               </div>
             </div>
           ))}
         </div>
       </div>
-      
     </div>
   );
 };
--- src/Components/ItemDetails/ItemDetails.jsx
+++ src/Components/ItemDetails/ItemDetails.jsx
@@ -1,13 +1,62 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const ItemDetails = ({ id }) => {
-  const [item, setItem] = useState();
-    useEffect(() => {
-      fetch(`http://localhost:4000/laptops`)
-      .then(res => res.json())
-      .then(data => setItem(data))
+const ItemDetails = () => {
+  const { id } = useParams();
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/laptops`)
+      .then((res) => res.json())
+      .then((data) => setItems(data));
   }, []);
-  return <div className="h-[80vh] bg-[#070707]"></div>;
+  const item = items.find((item) => item._id === id);
+  console.log(item);
+  return (
+    <div className="py-16 bg-[#070707]">
+      <div className="container mx-auto max-w-xl pt-6">
+        <img
+          className="h-96 w-full object-cover mx-auto rounded-md border-2 border-blue-500"
+          src={item?.image}
+          alt=""
+        />
+        <div className="mt-8">
+          <h2 className="text-white text-lg mb-5">
+            <span className="font-bold text-blue-600">Name:</span> {item?.name}
+          </h2>
+          <p className="text-white text-lg mb-5">
+            <span className="font-bold text-blue-600">Id:</span> {item?._id}
+          </p>
+          <p className="text-white text-lg mb-5">
+            <span className="font-bold text-blue-600">Price:</span> $
+            {item?.price}
+          </p>
+          <p className="text-white text-lg mb-5">
+            <span className="font-bold text-blue-600">Quantity:</span>{" "}
+            {item?.quantity}
+          </p>
+          <p className="text-white text-lg mb-5">
+            <span className="font-bold text-blue-600">Supplier Name:</span>{" "}
+            {item?.supplierName}
+          </p>
+          <p className="text-white text-lg">
+            <span className="font-bold text-blue-600">Description:</span>{" "}
+            {item?.description}
+          </p>
+          <div className="text-center">
+            <button className="text-white mt-6 py-2 px-10 bg-blue-600">
+              Delivered
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="text-center mt-12">
+        <form>
+          <input className="text-gray-600 w-[200px] py-2 pl-4 rounded outline-none" type="text" />
+          <input className=" py-2 px-8 ml-5 bg-slate-400 rounded" type="submit" value='Restock' />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ItemDetails;
