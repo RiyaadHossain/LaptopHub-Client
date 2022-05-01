c55615df2844a86787c472de4e51364481f47857
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-05-01 11:20:09 +0600

    Manage Items page added

--- src/Components/Inventory/Inventory.jsx
+++ src/Components/Inventory/Inventory.jsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { AiFillDelete } from 'react-icons/ai';
+import { AiFillDelete } from "react-icons/ai";
 
 const Inventory = () => {
   const [items, setItems] = useState([]);
-  const navigate = useNavigate();
+  const [isChange, setIsChange] = useState(false)
   useEffect(() => {
     fetch("http://localhost:4000/laptops")
       .then((res) => res.json())
       .then((data) => setItems(data));
-  }, []);
+  }, [isChange]);
+  const deleteItem = (id) => {
+    fetch(`http://localhost:4000/laptop/${id}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then(() => { setIsChange(!isChange)});
+  };
   return (
     <div className="py-16 bg-black">
       <div className=" container mx-auto">
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
           <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <thead class="text-xs text-gray-800 uppercase bg-gray-300 dark:bg-gray-900 dark:text-gray-400">
               <tr>
                 <th scope="col" class="px-6 py-3">
                   Image
@@ -36,29 +42,31 @@ const Inventory = () => {
             </thead>
 
             {items.map((item) => (
-              <tbody>
+              <tbody key={item._id}>
                 <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
-                  <td
-                    class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-                  >
-                    <img className="md:w-20 md:h-20 w-10 h-10 rounded-full object-cover" src={item.image} alt="" />
+                  <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                    <img
+                      className="md:w-20 border-2 border-blue-500 md:h-20 w-10 h-10 rounded-full object-cover"
+                      src={item.image}
+                      alt=""
+                    />
                   </td>
-                  <th class="px-6 py-4 font-bold text-gray-900 dark:text-white">{item.name}</th>
+                  <th class="px-6 py-4 font-bold text-gray-900 dark:text-white">
+                    {item.name}
+                  </th>
                   <td class="px-6 py-4">{item.quantity}</td>
                   <td class="px-6 py-4">${item.price}</td>
                   <td class="px-6 py-4 text-right">
                     <button
-                      
+                      onClick={() => deleteItem(item._id)}
                       class="font-medium text-2xl text-blue-600 dark:text-blue-500 hover:underline"
                     >
-                      <AiFillDelete/>
+                      <AiFillDelete />
                     </button>
                   </td>
                 </tr>
               </tbody>
             ))}
-
-            
           </table>
         </div>
       </div>
--- src/Components/ItemDetails/ItemDetails.jsx
+++ src/Components/ItemDetails/ItemDetails.jsx
@@ -1,64 +1,82 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ItemDetails = () => {
   const { id } = useParams();
   const [items, setItems] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     fetch(`http://localhost:4000/laptops`)
       .then((res) => res.json())
       .then((data) => setItems(data));
   }, []);
   const item = items.find((item) => item._id === id);
-  const deliveredItem = () => {
-   
-  }
+  const deliveredItem = () => {};
   return (
     <div className="py-16  bg-[#070707]">
       <div className="lg:flex container mx-auto">
-      <div className=" max-w-xl mx-auto pt-6">
-        <img
-          className="h-96 w-full object-cover mx-auto rounded-md border-2 border-blue-500"
-          src={item?.image}
-          alt=""
-        />
-        <div className="mt-8">
-          <h2 className="text-white text-lg mb-5">
-            <span className="font-bold text-blue-600">Name: </span> {item?.name}
-          </h2>
-          <p className="text-white text-lg mb-5">
-            <span className="font-bold text-blue-600">Id: </span> {item?._id}
-          </p>
-          <p className="text-white text-lg mb-5">
-            <span className="font-bold text-blue-600">Price: </span> $
-            {item?.price}
-          </p>
-          <p className="text-white text-lg mb-5">
-            <span className="font-bold text-blue-600">Quantity: </span>
-            {item?.quantity}
-          </p>
-          <p className="text-white text-lg mb-5">
-            <span className="font-bold text-blue-600">Supplier Name: </span>
-            {item?.supplierName}
-          </p>
-          <p className="text-white text-lg">
-            <span className="font-bold text-blue-600">Description: </span>
-            {item?.description}
-          </p>
-          <div className="text-center">
-            <button onClick={deliveredItem} className="text-white mt-6 py-2 px-10 bg-blue-600">
-              Delivered
-            </button>
+        <div className=" max-w-xl mx-auto pt-6">
+          <img
+            className="h-96 w-full object-cover mx-auto rounded-md border-2 border-blue-500"
+            src={item?.image}
+            alt=""
+          />
+          <div className="mt-8">
+            <h2 className="text-white text-lg mb-5">
+              <span className="font-bold text-blue-600">Name: </span>{" "}
+              {item?.name}
+            </h2>
+            <p className="text-white text-lg mb-5">
+              <span className="font-bold text-blue-600">Id: </span> {item?._id}
+            </p>
+            <p className="text-white text-lg mb-5">
+              <span className="font-bold text-blue-600">Price: </span> $
+              {item?.price}
+            </p>
+            <p className="text-white text-lg mb-5">
+              <span className="font-bold text-blue-600">Quantity: </span>
+              {item?.quantity}
+            </p>
+            <p className="text-white text-lg mb-5">
+              <span className="font-bold text-blue-600">Supplier Name: </span>
+              {item?.supplierName}
+            </p>
+            <p className="text-white text-lg">
+              <span className="font-bold text-blue-600">Description: </span>
+              {item?.description}
+            </p>
+            <div className="text-center">
+              <button
+                onClick={deliveredItem}
+                className="text-white mt-6 py-2 px-10 bg-blue-600"
+              >
+                Delivered
+              </button>
+            </div>
           </div>
         </div>
+        <div className="text-center mt-12">
+          <form>
+            <input
+              className="text-gray-600 w-[200px] py-2 pl-4 rounded outline-none"
+              type="text"
+            />
+            <input
+              className=" py-2 px-8 ml-5 bg-slate-400 rounded"
+              type="submit"
+              value="Restock"
+            />
+          </form>
+        </div>
       </div>
-      <div className="text-center mt-12">
-        <form>
-          <input className="text-gray-600 w-[200px] py-2 pl-4 rounded outline-none" type="text" />
-          <input className=" py-2 px-8 ml-5 bg-slate-400 rounded" type="submit" value='Restock' />
-        </form>
-        </div>
-        </div>
+      <div className="text-center">
+        <button
+          className="font-semibold hover:bg-blue-500 hover:text-white text-black mt-6 py-2 border-2 border-blue-500 px-10 bg-gray-300"
+          onClick={() => navigate("/inventory")}
+        >
+          Manage Inventory
+        </button>
+      </div>
     </div>
   );
 };
