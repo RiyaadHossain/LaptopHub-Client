e77be18eed39b5a297b024b80f3f809ac8e3f7e0
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-05-01 11:41:04 +0600

    Add Item added

--- src/Components/AddItem/AddItem.jsx
+++ src/Components/AddItem/AddItem.jsx
@@ -1,11 +1,90 @@
-import React from 'react';
+import React from "react";
 
 const AddItem = () => {
-    return (
-        <div className='h-[75vh] bg-black'>
-            
-        </div>
-    );
+    const addLaptop = (e) => {
+        e.preventDefault();
+        const brandName = e.target.brandName.value
+        const quantity = e.target.quantity.value
+        const price = e.target.price.value
+        const supplierName = e.target.supplierName.value
+        const description = e.target.description.value
+        const imageURL = e.target.image.value
+        console.log(brandName, quantity, price, supplierName, description, imageURL);
+    fetch("http://localhost:4000/laptop", {
+      method: "POST",
+      body: JSON.stringify({
+        name : brandName,
+        quantity : quantity,
+        price : price,
+        supplierName : supplierName,
+        description : description,
+        image : imageURL,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  };
+  return (
+    <div className=" bg-[#0c0c0c] py-24">
+      <div className="bg-slate-200 w-[500px] mx-auto py-12 px-3 rounded">
+        <form onSubmit={addLaptop}>
+          <h1 className="text-center font-semibold text-2xl">Add Item</h1>
+          <input
+            // onBlur={onEmailBlur}
+            className="block outline-none mt-4 py-3 w-full pl-3"
+            type="text"
+            name="brandName"
+            placeholder="Brand Name"
+          />
+
+          <input
+            // onBlur={onPasswordBlur}
+            className="block outline-none mt-4 py-3 w-full pl-3"
+            type="text"
+            name="quantity"
+            placeholder="Quantity"
+          />
+          <input
+            // onBlur={onPasswordBlur}
+            className="block outline-none mt-4 py-3 w-full pl-3"
+            type="text"
+            name="price"
+            placeholder="Price"
+          />
+          <input
+            // onBlur={onPasswordBlur}
+            className="block outline-none mt-4 py-3 w-full pl-3"
+            type="text"
+            name="supplierName"
+            placeholder="Supplier Name"
+          />
+          <input
+            // onBlur={onPasswordBlur}
+            className="block outline-none mt-4 py-3 w-full pl-3"
+            type="text"
+            name="description"
+            placeholder="Description"
+          />
+          <input
+            // onBlur={onPasswordBlur}
+            className="block outline-none mt-4 py-3 w-full pl-3"
+            type="text"
+            name="image"
+            placeholder="Image URL"
+          />
+
+          <input
+            className="bg-blue-500 py-2 mt-5 px-8 rounded text-slate-100"
+            type="submit"
+            value="Add"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddItem;
