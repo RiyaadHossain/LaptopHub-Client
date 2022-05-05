53f6e3cf7c289b911f0956878638722abdb2b2eb
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-05-05 12:56:34 +0600

    reviews added

--- src/Components/OurClient/OurClient.jsx
+++ src/Components/OurClient/OurClient.jsx
@@ -6,7 +6,9 @@ import Client3 from "../../Photo/person3.jpg";
 const OurClient = () => {
   return (
     <div className="bg-[#0b0b0b] py-28">
-      <h1 className="text-white text-4xl font-semibold text-center">Our Clients</h1>
+      <h1 className="text-white text-4xl font-semibold text-center">
+        Our Clients
+      </h1>
       <div className="container gap-11 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-[90px]">
         <div className="border relative text-center rounded-md bg-[#0e0d0d] py-6 px-2">
           <img
@@ -16,9 +18,9 @@ const OurClient = () => {
           />
           <div className="mt-14">
             <p className=" text-lg font-thin text-zinc-200">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-              repellendus repellat tempora quisquam veritatis aliquid aut error
-              voluptatibus. Aut, quam?
+              To be very honest, they supply the best laptops collection that I
+              have ever seen. The devices are completely fine, brand new and
+              super smooth to operate. I really appreciate their work.
             </p>
             <h2 className="text-blue-600 mt-5 text-xl font-bold">
               Sundar Pichai{" "}
@@ -37,9 +39,9 @@ const OurClient = () => {
           />
           <div className="mt-14">
             <p className=" text-lg font-thin text-zinc-200">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-              repellendus repellat tempora quisquam veritatis aliquid aut error
-              voluptatibus. Aut, quam?
+              Two weeks ago, I cheecked thir laptops from thier site and now I
+              along with my team running our technical tasks with their devices.
+              Also, my teammates gave me positive fedbacks about their products.
             </p>
             <h2 className="text-blue-600 mt-5 text-xl font-bold">
               Satya Nadella{" "}
@@ -58,9 +60,10 @@ const OurClient = () => {
           />
           <div className="mt-14">
             <p className=" text-lg font-thin text-zinc-200">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-              repellendus repellat tempora quisquam veritatis aliquid aut error
-              voluptatibus. Aut, quam?
+              Undoubtedly, they store all the best products. Their services
+              after providing main products also very well. The whole system
+              they run is very precise and time saving. They are doing really
+              great work.
             </p>
             <h2 className="text-blue-600 mt-5 text-xl font-bold">
               Leena Nair{" "}
